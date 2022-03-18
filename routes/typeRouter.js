c411299f2080ccf073ebab84f6e29fa8ce068bb5
alexei <alexei3017@mail.ry>
2022-03-18 12:30:55 +0300

    feat(routes): создал routes, подключил все роуты в index.js routes

---
+++ routes/typeRouter.js
@@ -0,0 +1,12 @@
+const Router = require('express');
+const router = new Router();
+
+router.post('/', (req, res) => {
+
+});
+
+router.get('/', (req, res) => {
+
+})
+
+module.exports = Router;
---
+++ routes/userRouter.js
@@ -0,0 +1,15 @@
+const Router = require('express');
+const router = new Router();
+
+router.post('/registration', (req, res) => {
+
+});
+router.post('/login', (req, res) => {
+
+});
+
+router.get('/auth', (req, res) => {
+
+})
+
+module.exports = Router;
