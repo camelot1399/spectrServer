fcc2bf947a9d62406951849fe14d6016fca06152
alexei <alexei3017@mail.ry>
2022-03-18 11:39:36 +0300

    models, db. env

---
+++ index.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+const express = require('express');
+const sequelize = require('./db');
+
+const PORT = process.env.PORT || 7000;
+const app = express();
+
+const start = async () => {
+    try {
+        await sequelize.authenticate();
+        await sequelize.sync();
+        app.listen(PORT, () => {
+            console.log('server start on ' + PORT + ' port');
+        });
+    } catch(e) {
+        console.log('error', e);
+    }
+}
+
+start();
