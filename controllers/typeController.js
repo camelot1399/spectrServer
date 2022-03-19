1a2e6b550fbe41febefe514576293caaa5e11c97
alexei <alexei3017@mail.ry>
2022-03-19 20:05:30 +0300

    feat(routes): controllers is done

--- controllers/typeController.js
+++ controllers/typeController.js
@@ -10,7 +10,9 @@ class TypeController {
     }
 
     async getAll(req, res) {
+        const types = await Type.findAll();
 
+        return res.json(types);
     }
 }
 
--- models/models.js
+++ models/models.js
@@ -63,7 +63,7 @@ BasketDevice.belongsTo(Basket);
 Device.hasMany(Rating);
 Rating.belongsTo(Device);
 
-Device.hasMany(DeviceInfo);
+Device.hasMany(DeviceInfo, {as: 'info'});
 DeviceInfo.belongsTo(Device);
 
 Type.hasMany(Device);
